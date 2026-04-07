860525205a9c4f0064572da6c6b3f096591ed010
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-08 00:27:38 +0700

    chore: add link routes

--- Backend/internal/routes/routes.go
+++ Backend/internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"koda-b6-url-shortener/internal/di"
+	"koda-b6-url-shortener/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -11,11 +12,18 @@ func SetupRoutes(r *gin.Engine, conn *pgxpool.Pool) {
 	container := di.NewContainer(conn)
 
 	userHandler := container.UserHandler()
+	linkHandler := container.LinkHandler()
 
 	a := r.Group("/api")
 	{
 		a.POST("/register", userHandler.Create)
 		a.POST("/login", userHandler.Login)
-		a.GET("/:id", userHandler.GetById)
+		l := a.Group("/links", middleware.AuthMiddleware())
+		{
+			l.POST("", linkHandler.CreateLink)
+			l.GET("", linkHandler.GetUserLinks)
+			l.DELETE("/:id", linkHandler.DeleteLink)
+		}
 	}
+	r.GET("/:slug", linkHandler.Redirect)
 }
