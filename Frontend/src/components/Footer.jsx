c5306ebfe90f5850afa23cef51dd5518262df8ab
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-07 14:24:08 +0700

    chore: init Footer component

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+export const Footer = () => {
+    return(
+        <div className="flex justify-between items-center w-full h-16 px-5 bg-white">
+            <span className="text-gray-800">@ 2026 SHORTLINK. THE DIGITAL ARCHITECT.</span>
+            <ul className="text-gray-800 flex gap-6">
+                <li>PRIVACY POLICY</li>
+                <li>TERMS OF SERVICE</li>
+                <li>API DOCUMENTATION</li>
+                <li>SUPPORT</li>
+            </ul>
+        </div>
+    )
+}
