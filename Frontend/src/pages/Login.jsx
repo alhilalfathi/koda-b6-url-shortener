cd543a8554e8ffb31e5ce4fcb4567acc22a748a3
alhilalfathi <alhilal.fathi@gmail.com>
2026-04-07 14:25:03 +0700

    chore: init login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,79 @@
+import { useForm } from "react-hook-form"
+import { InputForm } from "../components/InputForm"
+import { useState } from "react"
+
+//import icons
+import { HiOutlineMail } from "react-icons/hi"
+import { GoKey } from "react-icons/go"
+import { FiEye } from "react-icons/fi"
+import { Footer } from "../components/Footer"
+import { Link } from "react-router-dom"
 
 
-export const Login = () => {
+export const LoginPage = () => {
+    const { handleSubmit, register } = useForm()
 
-    return(
-        <>
-        
-        </>
+    //show-hide password
+    const [changePassword, setChangePassword] = useState(true)
+    const changeIcon = changePassword === true ? false : true
+
+    function submitForm() {
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(submitForm)} className="flex flex-col items-center bg-[#f7f8fa] justify-center min-w-screen">
+                <h1 className="flex text-2xl font-extrabold text-black mt-10">ShortLink</h1>
+                
+                {/* whitebox */}
+                <div className="flex flex-col gap-5 items-center bg-white w-120 h-125 py-10 mt-5 mb-5 rounded">
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-extrabold">Welcome Back</span>
+                        <span>Please enter your details to sign in.</span>
+                    </div>
+                    <InputForm
+                        type={"email"}
+                        id={"email"}
+                        name={"email"}
+                        icon={<HiOutlineMail />}
+                        register={register}
+                        placeholder={"Enter Your Email"}
+                    >
+                        Email
+                    </InputForm>
+                    <InputForm
+                        type={changePassword ? "password" : "text"}
+                        id={"password"}
+                        name={"password"}
+                        icon={<GoKey />}
+                        register={register}
+                        placeholder={"Enter Your Password"}
+                        eye={<FiEye className="cursor-pointer" onClick={() => { setChangePassword(changeIcon) }} />}
+                    >
+                        Password
+                    </InputForm>
+
+                    {/* login button */}
+                    <button
+                        className="w-2/3 px-8 py-3 bg-blue-700 text-white rounded-xl"
+                        type="submit"
+                    >
+                        Log In
+                    </button>
+
+                    <span className="text-gray-600">OR CONTINUE WITH</span>
+
+                    {/* login with google */}
+                    <button
+                        className="w-2/3 px-8 py-3 border border-black rounded-xl"
+                    >
+                        Sign in with Google
+                    </button>
+                </div>
+                <span className="mb-5">Dont have an account? <Link to="/register" className="text-blue-800">Sign up</Link></span>
+            </form>
+            <Footer />
+        </div>
     )
 }
